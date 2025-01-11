2fb2d0bd7aedaf079ca3b0f8a7ca3013a2b60e32
ihasp <patrykihas9@gmail.com>
2025-01-11 18:30:56 +0100

    t

--- app/scanner/index.tsx
+++ app/scanner/index.tsx
@@ -15,6 +15,7 @@ export default function Home() {
   const qrLock = useRef(false);
   const appState = useRef(AppState.currentState);
 
+
   useEffect(() => {
     const sub = AppState.addEventListener("change", (nextAppState) => {
       if (
